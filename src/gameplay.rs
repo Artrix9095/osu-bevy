749568a05d3580894f4ae159b635771a452792b1
Artrix9095 <sefu786@outlook.com>
2025-01-25 22:51:00 -0800

    Attempt to add better hitobjects

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -9,7 +9,12 @@ impl Plugin for GameplayPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (tick_game_timer, handle_music_start, spawn_hitobjects),
+            (
+                tick_game_timer,
+                handle_music_start,
+                spawn_hitobjects,
+                update_hitobjects,
+            ),
         )
         .init_resource::<GameState>();
     }
@@ -19,14 +24,16 @@ impl Plugin for GameplayPlugin {
 pub struct GameState {
     timer: Stopwatch,
     music_started: bool,
-    current_object_index: usize,
+    spawned_objects: bool,
 }
 
 #[derive(Component)]
-pub struct HitCircle {
-    spawn_time: f32,
-    position: Vec2,
-}
+pub struct HitObject(
+    /// Approach rate window
+    pub f32,
+    /// Hit window
+    pub f32,
+);
 
 fn tick_game_timer(time: Res<Time>, mut game_state: ResMut<GameState>) {
     game_state.timer.tick(time.delta());
@@ -36,11 +43,10 @@ fn handle_music_start(
     mut game_state: ResMut<GameState>,
     mut audio_query: Query<&mut AudioSink, With<MasterAudio>>,
 ) {
-    dbg!("{:#?}", &game_state);
     if !game_state.music_started {
-        dbg!("handle_music_start");
         if let Ok(mut playback) = audio_query.get_single_mut() {
             playback.play();
+
             game_state.music_started = true;
         }
     }
@@ -61,38 +67,43 @@ fn spawn_hitobjects(
 
     if let Some(beatmap) = beatmap_assets.get(&beatmap_resource.beatmap) {
         let hit_objects = &beatmap.0.hit_objects;
+        let ar = &beatmap.0.approach_rate;
+        let od = &beatmap.0.overall_difficulty;
 
-        while game_state.current_object_index < hit_objects.len() {
-            let hit_object = &hit_objects[game_state.current_object_index];
-            let current_time = time.elapsed_secs();
-
-            // Calculate spawn time based on approach rate
-            let ar = beatmap.0.approach_rate;
-            let preempt = ar_to_ms(ar);
-
-            if current_time >= hit_object.start_time as f32 - preempt {
-                match hit_object.kind {
-                    HitObjectKind::Circle(hit_object) => {
-                        let position = Vec2::new(hit_object.pos.x, hit_object.pos.y);
-                        let circle = meshes.add(Circle::new(10.0));
-                        commands.spawn((
-                            HitCircle {
-                                spawn_time: current_time,
-                                position,
-                            },
-                            Mesh2d(circle),
-                            MeshMaterial2d(materials.add(Color::rgb(0.0, 0.0, 1.0))),
-                            Transform::from_xyz(position.x, position.y, 3.0),
-                            // Add any other components needed for rendering
-                        ));
-
-                        game_state.current_object_index += 1;
-                    }
-                    _ => {}
+        for (_, hit_object) in hit_objects.iter().enumerate() {
+            match hit_object.kind {
+                HitObjectKind::Circle(circle) => {
+                    commands.spawn((
+                        HitObject(hit_object.start_time as f32 - ar_to_ms(*ar), 0.0),
+                        Mesh2d(meshes.add(Circle::new(10.0))),
+                        MeshMaterial2d(materials.add(Color::srgba(0.0, 0.0, 1.0, 0.0))),
+                        Transform::from_xyz(circle.pos.x, circle.pos.y, 3.0),
+                    ));
                 }
-            } else {
-                break;
+                _ => {}
             }
+        }
+        game_state.spawned_objects = true;
+    }
+}
+
+pub fn update_hitobjects(
+    mut query: Query<(&mut HitObject, &mut MeshMaterial2d<ColorMaterial>)>,
+    time: Res<Time>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+
+    mut game_state: ResMut<GameState>,
+) {
+    if !game_state.spawned_objects {
+        return;
+    }
+
+    for (mut hit_object, mut mesh) in query.iter_mut() {
+        let delta = hit_object.0 - time.delta_secs();
+        if delta > 0.0 {
+            mesh.0 = materials.add(Color::srgba(0.0, 0.0, 1.0, 1.0));
+        } else {
+            mesh.0 = materials.add(Color::srgba(0.0, 0.0, 1.0, 0.0));
         }
     }
 }
